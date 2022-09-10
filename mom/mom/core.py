b01f101fc0981004ecf80afdfb2afaec1ceff77d
Florian Feurstein <florian.feurstein@mksinst.com>
2022-09-10 14:02:38 +0200

    changes

--- mom/mom/core.py
+++ mom/mom/core.py
@@ -4,14 +4,30 @@ import time
 
 import gmqtt
 
-z_minimum = 0
+z_minimum = 7
 z_maximum = 100
 
 positions = [
     {
-        'from': (0, 0, 0, 0),
-        'to': (50, 60, 0, 30)
-    }
+        'from': (50, 50, 0, 90),
+        'to': (150, 160, 0, 30)
+    },
+    {
+        'from': (150, 160, 0, 120),
+        'to': (50, 50, 0, 0)
+    },
+    # {
+    #     'from': (50, 150, 0, 0),
+    #     'to': (150, 50, 0, 90)
+    # },
+    # {
+    #     'from': (150, 160, 0, 120),
+    #     'to': (50, 150, 0, 0)
+    # },
+    # {
+    #     'from': (150, 50, 0, 0),
+    #     'to': (50, 50, 0, 0)
+    # }
 ]
 
 
@@ -36,7 +52,7 @@ async def run(broker_host: str):
     loop.add_signal_handler(signal.SIGTERM, stop)
 
     client.publish('pick_n_plaser/stop', False)
-    publish_coordinates(client, 0, 0, 0, 0)
+
 
     for pos in positions:
         move_item(client, pos['from'], pos['to'])
@@ -55,17 +71,13 @@ def pick_up(client, x_position, y_position, rotation):
 def release(client, x_position, y_position, rotation):
     publish_coordinates(client, x_position, y_position, z_minimum, rotation)
     publish_coordinates(client, x_position, y_position, z_minimum, (rotation + 90) % 180)
-    publish_coordinates(client, x_position, y_position, z_maximum, 0)
+    publish_coordinates(client, x_position, y_position, z_maximum, (rotation + 90) % 180)
 
 
 def move_item(client, from_position, to_position):
     from_x, from_y, from_z, from_rotation = from_position
     to_x, to_y, to_z, to_rotation = to_position
-    time.sleep(1)
-    publish_coordinates(client, from_x, from_y, from_z, from_rotation)
-    time.sleep(1)
+    publish_coordinates(client, from_x, from_y, z_maximum, from_rotation)
     pick_up(client, from_x, from_y, from_rotation)
-    time.sleep(1)
     publish_coordinates(client, to_x, to_y, z_maximum, to_rotation)
-    time.sleep(1)
     release(client, to_x, to_y, to_rotation)
